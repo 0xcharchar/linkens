0437532f0557c0ae612a5106785f391992ab4109
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-09-25 00:26:31 +0100

    single repo for site and contracts

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -18,5 +18,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4"
+  solidity: "0.8.4",
+  paths: {
+    sources: './src',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts'
+  }
 };
