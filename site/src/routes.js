cae7418847226f55b7ca2e17bf0f6d3a6a514461
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-09-25 16:21:25 +0100

    supporters landing page

--- site/src/routes.js
+++ site/src/routes.js
@@ -1,7 +1,9 @@
 import Landing from './views/landing.svelte'
+import Search from './views/search.svelte'
 
 const routes = [
-  { name: '/', component: Landing }
+  { name: '/', component: Landing },
+  { name: '/supporters', component: Search },
 ]
 
 export { routes }
---
+++ site/src/views/search.svelte
@@ -0,0 +1,51 @@
+<script>
+  import SearchResult from '../components/search-result.svelte'
+
+  let searchResults = [
+    {
+      name: 'Tim',
+      avatar: '/man1.jpeg',
+      url: '/user/tim',
+      twitter: 'tim973982'
+    },
+    {
+      name: 'Bob',
+      avatar: '/man2.jpeg',
+      url: '/user/bob',
+      twitter: 'bob973982'
+    },
+    {
+      name: 'Alice',
+      avatar: '/woman1.jpeg',
+      url: '/user/alice',
+      twitter: 'alice973982'
+    },
+    {
+      name: 'Janet',
+      avatar: '/woman2.jpeg',
+      url: '/user/janet',
+      twitter: 'janet973982'
+    }
+  ]
+</script>
+
+<main>
+  <h1><em>Support</em> your favourite creators</h1>
+  <h2>Featured</h2>
+
+  <ul>
+    {#each searchResults as profile}
+      <SearchResult profile={profile} />
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+    padding: 1rem 10rem;
+  }
+
+  h1 em {
+    font-style: bold;
+  }
+</style>
