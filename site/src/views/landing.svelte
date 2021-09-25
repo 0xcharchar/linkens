b990bfcfc3207d0046e9989b6b832b352b15de89
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-09-25 15:49:59 +0100

    landing page layout mostly completed

--- site/src/views/landing.svelte
+++ site/src/views/landing.svelte
@@ -38,6 +38,26 @@
       <img src="https://picsum.photos/400/200?grayscale">
     </div>
   </section>
+
+  <section id="footer">
+    <div>
+      <img src="/dummy-logo.png" width="100" height="100">
+    </div>
+
+    <div>
+      <p>My grasp on reality right now is tenuous. We've got kpis for that we need to crystallize a plan. Shotgun approach value prop. First-order optimal strategies I have zero cycles for this, so are there any leftovers in the kitchen?</p>
+    </div>
+
+    <div>
+      <p>Links</p>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Creators</a></li>
+        <li><a href="#">Supporters</a></li>
+      </ul>
+    </div>
+  </section>
 </main>
 
 <style>
@@ -64,6 +84,16 @@
   }
 
   #hero p {
-    font-size: 1.5rem;
+    font-size: 1.2rem;
+  }
+
+  #footer {
+    background: #3a3a3a;
+    padding: 1.5rem 18rem;
+    color: #efefef;
+  }
+
+  #footer div {
+    margin: 2rem;
   }
 </style>
