21261fb5b5778a8db703e6f892d08fdecc1cc754
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-09-25 01:05:51 +0100

    WIP landing page layout

---
+++ site/src/routes.js
@@ -0,0 +1,7 @@
+import Landing from './views/landing.svelte'
+
+const routes = [
+  { name: '/', component: Landing }
+]
+
+export { routes }
---
+++ site/src/views/landing.svelte
@@ -0,0 +1,69 @@
+<script>
+</script>
+
+<main>
+  <section id="hero">
+    <div>
+      <h1>Site name</h1>
+      <p>This is not linktr.ee</p>
+      <button>For Creators</button>
+      <button>For Supporters</button>
+    </div>
+
+    <div>
+      <img src="https://picsum.photos/600/300?grayscale">
+    </div>
+  </section>
+
+  <section id="for-creators">
+    <div>
+      <img src="https://picsum.photos/400/200?grayscale">
+    </div>
+
+    <div class="description">
+      <h3>For creators</h3>
+      <p>It's not hard guys corporate synergy. Moving the goalposts value-added touch base killing it, for going forward. This is not the hill i want to die on 4-blocker what the zeitgeist but high performance keywords or product launch. Get buy-in. Level the playing field fire up your browser, drop-dead date.</p>
+      <button>Let's build!</button>
+    </div>
+  </section>
+
+  <section id="for-supporters">
+    <div class="description">
+      <h3>For supporters</h3>
+      <p>I need to pee and then go to another meeting sorry i didn't get your email. Window-licker closer to the metal and going forward. We need to get all stakeholders up to speed and in the right place race without a finish line sorry i was triple muted and form without content style without meaning yet obviously but we're ahead of the curve on that one but digital literacy.</p>
+      <button>Find your favourite creator</button>
+    </div>
+
+    <div>
+      <img src="https://picsum.photos/400/200?grayscale">
+    </div>
+  </section>
+</main>
+
+<style>
+  main {
+    padding: 0;
+    margin: 0;
+  }
+
+  section {
+    width: 100%;
+    padding: 5rem 10rem;
+    margin: 0 0 1rem 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .description {
+    max-width: 60ch;
+  }
+
+  #hero {
+    background: #dedede;
+  }
+
+  #hero p {
+    font-size: 1.5rem;
+  }
+</style>
