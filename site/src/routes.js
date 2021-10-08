05610bf04486a0909299e6a443c8fd8124bdb35f
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-10-08 23:58:41 +0100

    remove dead code

--- site/src/routes.js
+++ site/src/routes.js
@@ -1,14 +1,10 @@
 import { wrap } from 'svelte-spa-router/wrap'
 
 import Landing from './views/landing.svelte'
-import Search from './views/search.svelte'
-import User from './views/user.svelte'
 import Wizard from './views/wizard.svelte'
 
 const routes = {
   '/': Landing,
-  '/supporters': Search,
-  '/user': User,
   '/wizard': Wizard,
 }
 
