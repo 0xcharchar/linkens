b3586edaccecd7efa7479075ae90b43a06bdcf3c
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-10-10 05:20:18 +0100

    add a warning banner that this is only available on ropsten

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -5,4 +5,19 @@
 	export let name;
 </script>
 
+<div id="warning">
+  <p>This application only works on the Ropsten testnet</p>
+</div>
+
 <Router {routes} />
+
+<style>
+  #warning {
+    background: #c76b6b;
+    font-size: 1em;
+    font-weight: bold;
+    color: #fff;
+    padding: 0.7em;
+    text-align: center;
+  }
+</style>
