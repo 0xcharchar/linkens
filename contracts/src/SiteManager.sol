b1d289500c7c9df9c2f8a74372c2d4f9ce181499
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-10-22 16:03:02 +0100

    fix ens deployment for test suite

--- contracts/src/SiteManager.sol
+++ contracts/src/SiteManager.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@ensdomains/ens-contracts/contracts/resolvers/PublicResolver.sol";
 import "@ensdomains/ens-contracts/contracts/registry/ENS.sol";
 
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract SiteManager {
   ENS private immutable s_ens;
-  PublicResolver private immutable s_resolver;
   bytes32 private immutable s_manageNode;
+  PublicResolver private immutable s_resolver;
 
   event SubdomainRegistered (address owner, bytes32 indexed label);
 
@@ -20,11 +20,16 @@ contract SiteManager {
     s_manageNode = _manageNode;
   }
 
+  /**
+    * Create a subnode for the managed node and update the resolver with
+    * with function calls from `data`. Finally, set the owner of the subnode
+    * to be the `msg.sender`
+    */
   function subdomainRegister (bytes32 label, bytes[] calldata data) external returns (bytes[] memory results) {
-    // Create the subdomain through this contract so that it is the owner
+    // Create the subdomain through this contract so that the contract is the owner, temporarily
     s_ens.setSubnodeRecord(s_manageNode, label, address(this), address(s_resolver), 500);
 
-    // Update all the records
+    // Update all the records by calling the current default resolver
     results = new bytes[](data.length);
     for (uint i = 0; i < data.length; i++) {
       (bool success, bytes memory result) = address(s_resolver).call(data[i]);
--- contracts/test/SiteManager.test.js
+++ contracts/test/SiteManager.test.js
@@ -1,33 +1,52 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { labelhash, namehash } = require('@ensdomains/ensjs')
-const { ENSRegistry, FIFSRegistrar } = require('@ensdomains/ens-contracts')
+const { ENSRegistry } = require('@ensdomains/ens-contracts')
 
-async function deployEns (root = 'ethonline2021char') {
-  const ENS = await ethers.getContractFactory('ENSDeployer')
-  const ens = await ENS.deploy()
-  await ens.deployed();
+const TLD = 'eth'
+const DOMAIN = 'linkens'
 
+const domain = () => `${DOMAIN}.${TLD}`
+
+async function ensDeployer () {
   const [owner] = await ethers.getSigners()
-  const registrar = new ethers.Contract(await ens.fifsRegistrar(), FIFSRegistrar, owner)
 
-  const tx = await registrar.connect(owner).register(labelhash(root), await owner.getAddress())
-  await tx.wait()
+  // deploy registry
+  const ensFactory = await ethers.getContractFactory('ENSRegistry')
+  const ens = await ensFactory.deploy()
+
+  // deploy resolver
+  const resolverFactory = await ethers.getContractFactory('PublicResolver')
+  const resolver = await resolverFactory.deploy(ens.address, ethers.constants.AddressZero)
+
+  // magic base resolver
+  const resolverNode = namehash('resolver')
+  const resolverLabel = ethers.utils.id('resolver')
+  await ens.setSubnodeOwner(ethers.constants.HashZero, resolverLabel, await owner.getAddress())
+  await ens.setResolver(resolverNode, resolver.address)
+  await resolver['setAddr(bytes32,uint256,bytes)'](resolverNode, 60, resolver.address)
+
+  // create TLD
+  await ens.setSubnodeOwner(ethers.constants.HashZero, ethers.utils.id(TLD), await owner.getAddress())
 
   return {
-    registryAddr: await ens.ens(),
-    resolverAddr: await ens.publicResolver()
+    registry: ens,
+    resolver: resolver
   }
 }
 
-async function deploySiteManager (registryAddr, resolverAddr, root = 'ethonline2021char') {
+async function deploySiteManager (registry, resolver) {
+  const [owner] = await ethers.getSigners()
+
+  // create domain record
+  await registry.setSubnodeOwner(namehash(TLD), labelhash(DOMAIN), await owner.getAddress())
+
+  // deploy SiteManager
   const SiteManager = await ethers.getContractFactory('SiteManager')
-  const sm = await SiteManager.deploy(registryAddr, resolverAddr, namehash(`${root}.eth`))
+  const sm = await SiteManager.deploy(registry.address, resolver.address, namehash(`${domain()}`))
   await sm.deployed()
 
-  const [owner] = await ethers.getSigners()
-  const registry = new ethers.Contract(registryAddr, ENSRegistry, owner)
-
+  // allow SiteManager to make calls on behalf of owner
   const approveTx = await registry.connect(owner).setApprovalForAll(sm.address, true)
   await approveTx.wait()
 
@@ -36,22 +55,16 @@ async function deploySiteManager (registryAddr, resolverAddr, root = 'ethonline2
 
 describe('SiteManager', function () {
   it('should be able to register a subdomain', async function () {
-    const { registryAddr, resolverAddr } = await deployEns()
-    const siteManager = await deploySiteManager(registryAddr, resolverAddr)
+    const { registry, resolver } = await ensDeployer()
+    const siteManager = await deploySiteManager(registry, resolver)
 
     const [, testAcct] = await ethers.getSigners()
-    const regTx = await siteManager.connect(testAcct).subdomainRegister(labelhash('test'))
+    const regTx = await siteManager.connect(testAcct).subdomainRegister(labelhash('test'), [])
     await regTx.wait()
 
-    const registry = new ethers.Contract(registryAddr, ENSRegistry, testAcct)
-    const nodeOwner = await registry.connect(testAcct).owner(namehash('test.ethonline2021char.eth'))
+    const nodeOwner = await registry.connect(testAcct).owner(namehash(`test.${domain()}`))
 
     // the person who requested the subdomain is the owner
     expect(nodeOwner).to.be.eq(await testAcct.getAddress())
-
-    // TODO can this be done from SiteManager?
-    // the SiteManager should be the controller (operator in contract terms)
-    const mgrIsController = await registry.connect(testAcct).isApprovedForAll(nodeOwner, await testAcct.getAddress())
-    expect(mgrIsController).to.be.true
   })
 })
