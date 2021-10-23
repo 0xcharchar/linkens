e8b9f498172d6e56f6f4b67de577bd965a872ba8
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-10-23 17:34:24 +0100

    Post hackathon (#1)
    * Packages updated
    * Narrowed down dependencies (took on lots while exploring solutions)
    * Updated READMEs
    * Documented contracts and added a couple features
    * Fixed testing contracts
    * WIP new design for wizard and user pages

--- contracts/src/SiteManager.sol
+++ contracts/src/SiteManager.sol
@@ -5,29 +5,50 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@ensdomains/ens-contracts/contracts/resolvers/PublicResolver.sol";
 import "@ensdomains/ens-contracts/contracts/registry/ENS.sol";
 
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
-contract SiteManager {
+contract SiteManager is Ownable {
+  // Reference to a registry that matches the ENS interface
   ENS private immutable s_ens;
-  PublicResolver private immutable s_resolver;
+
+  // The domain to create subdomains from, this contract needs to be the owner or an operator
   bytes32 private immutable s_manageNode;
+
+  // Default resolver to use when creating subdomains
+  address public s_defaultResolver;
 
   event SubdomainRegistered (address owner, bytes32 indexed label);
 
-  constructor(ENS _registryAddr, PublicResolver _resolverAddr, bytes32 _manageNode) {
+  constructor(ENS _registryAddr, address _resolverAddr, bytes32 _manageNode) {
     s_ens = _registryAddr;
-    s_resolver = _resolverAddr;
+    s_defaultResolver = _resolverAddr;
     s_manageNode = _manageNode;
   }
 
-  function subdomainRegister (bytes32 label, bytes[] calldata data) external returns (bytes[] memory results) {
-    // Create the subdomain through this contract so that it is the owner
-    s_ens.setSubnodeRecord(s_manageNode, label, address(this), address(s_resolver), 500);
+  /**
+    * Currently risky in that any contract address could be passed in and this blows
+    * up later during a subdomain registration
+    * TODO verification by interface ids?
+    */
+  function setDefaultResolver (address resolver) external onlyOwner {
+    s_defaultResolver = resolver;
+  }
 
-    // Update all the records
+  /**
+    * Create a subnode for the managed node and update the resolver with
+    * with function calls from `data`. Finally, set the owner of the subnode
+    * to be the `msg.sender`
+    */
+  function subdomainRegister (bytes32 label, bytes[] calldata data) external returns (bytes[] memory results) {
+    address resolverAddr = s_defaultResolver;
+
+    // Create the subdomain through this contract so that the contract is the owner, temporarily
+    s_ens.setSubnodeRecord(s_manageNode, label, address(this), resolverAddr, 500);
+
+    // Update all the records by calling the current default resolver
     results = new bytes[](data.length);
     for (uint i = 0; i < data.length; i++) {
-      (bool success, bytes memory result) = address(s_resolver).call(data[i]);
+      (bool success, bytes memory result) = resolverAddr.call(data[i]);
       require(success, "Failed calling to ENS resolver");
       results[i] = result;
     }
--- site/src/routes.js
+++ site/src/routes.js
@@ -2,10 +2,12 @@ import { wrap } from 'svelte-spa-router/wrap'
 
 import Landing from './views/landing.svelte'
 import Wizard from './views/wizard.svelte'
+import User from './views/user.svelte'
 
 const routes = {
   '/': Landing,
   '/wizard': Wizard,
+  '/user': User
 }
 
 export { routes }
